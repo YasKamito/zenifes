7b405d19d2f5d0869f1ea88ac54fefaf1a28fc89
chimo <koala@koa-labo.com>
2019-04-19 23:42:29 +0900

    Fix

--- themes/twentynineteen-child/footer.php
+++ themes/twentynineteen-child/footer.php
@@ -12,10 +12,10 @@
 		<div class="site-info -align--center">
 			<?php $blog_info = get_bloginfo( 'name' ); ?>
 			<?php if ( ! empty( $blog_info ) ) : ?>
-				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="ZENIBAKO FES"/></a>,
+				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="ZENIBAKO FES"/></a>
 			<?php endif; ?>
 			<a class="link-contact -block -text--white -font-size--medium -font-bold" href="<?php echo esc_url( home_url( '/' ) ); ?>contact" rel="contact">お問い合わせはこちら</a>
-			
+
 			<div class="privacy-policy -text--white -font-size--small">
 				<div>&copy; ZENIBAKOFES All Rights Reserved.</div>
 				<div>※当サイトに掲載された文章や画像等の著作物を無断で転用・掲載することを禁止いたします。</div>
